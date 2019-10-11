05de7f6a32cd82cec53f1d445fe0ff8adac39174
Warrior87 <buerk1a@cmich.edu>
2019-10-11 17:01:22 -0400

    Added OTA functionality
    added the ability to now update code over the air. finally got rid of the cable

--- Sunrise_Alarm/Sunrise_Alarm.ino
+++ Sunrise_Alarm/Sunrise_Alarm.ino
@@ -24,7 +24,9 @@
  */
 
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <TimeLib.h>
 #include <BlynkSimpleEsp8266.h>
 //#define DEBUG
@@ -245,6 +247,7 @@ void setup() {
   #ifdef DEBUG
     Serial.begin(115200);
   #endif
+    WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, pass);
   while (WiFi.status() != WL_CONNECTED) {
     analogWrite(bluePin, 10);                    /*lights will blink blue while connecting to wifi*/
@@ -255,6 +258,38 @@ void setup() {
       Serial.print(".");
     #endif
   }
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 
   Blynk.begin(auth, ssid, pass);
   //Blynk.run();  
@@ -275,6 +310,7 @@ void setup() {
 }
 
 void loop() {         //////////////////////////////////////////////see how often we loop through the loop() function///////////////////////////////////////////////////////
+  ArduinoOTA.handle();
   Blynk.run();
 
   //this updates the current time every second for an hour, so the server is only pinged once an hour
